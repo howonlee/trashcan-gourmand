c494387442d8ed015305221a6c7795ba6748975e
howon lee <hlee.howon@gmail.com>
2019-12-02 07:09:09 -0800

    actually doing html lol

--- emailer.py
+++ emailer.py
@@ -7,17 +7,12 @@ import functools
 from settings import Settings
 from typing import List, Union
 
-def send_message(date: datetime.date, settings: Settings) -> None:
-    ##########
-    ##########
-    ########## need the logic for attachment...
-    ##########
-    ##########
+def send_message(contents: str, date: datetime.date, settings: Settings) -> None:
     msg_obj: email.message.EmailMessage = email.message.EmailMessage()
     msg_obj['From'] = settings.smtp_username
     msg_obj['To'] = settings.smtp_dest_email
     msg_obj['Subject'] = "Trashcan Gourmand | {}".format(str(date))
-    # msg_obj.set_content(contents)
+    msg_obj.set_content(contents)
     with smtplib.SMTP(settings.smtp_url, settings.smtp_port) as server:
         server.ehlo()
         server.starttls()
