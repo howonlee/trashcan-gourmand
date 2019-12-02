5fee2d6ed946630c392a652b7d64e352d33b6894
howon lee <hlee.howon@gmail.com>
2019-12-02 08:12:42 -0800

    consolidate into one file

--- trash_cli.py
+++ trash_cli.py
@@ -4,11 +4,33 @@ import time
 import datetime
 import crontab
 import click
-from typing import Union, List
+from typing import Union, List, Dict, Any
 import email
 import smtplib
 import random
 import functools
+import dataclasses
+import json
+
+@dataclasses.dataclass
+class Settings(object):
+    root_dir: str # Root dir to sample the files from
+    filetypes: Union[str, List[str]] # filetype or filetypes to sample from
+    smtp_username: str
+    smtp_password: str
+    smtp_dest_email: str
+    smtp_url: str = "smtp.gmail.com"
+    smtp_port: int = 587
+
+    def to_file(self, settings_filename: str) -> None:
+        with open(settings_filename, "w") as settings_file:
+            json.dump(dataclasses.asdict(self), settings_file)
+
+    @staticmethod
+    def from_file(settings_filename: str):
+        with open(settings_filename, "r") as settings_file:
+            json_res: Dict[str, Any] = json.load(settings_file)
+            return Settings(**json_res)
 
 def send_message(contents: str, date: datetime.date, settings: Settings) -> None:
     msg_obj: email.message.EmailMessage = email.message.EmailMessage()
