d437db3627e9eb6b482b68cd5c5c3a624c16159b
howon lee <hlee.howon@gmail.com>
2019-12-02 07:11:57 -0800

    add the click bits

--- trash_cli.py
+++ trash_cli.py
@@ -1,22 +1,40 @@
+#!/usr/bin/env python3.7
+import datetime
+import crontab
+import click
 
+@click.group()
+def cli():
+    pass
 
+@cli.command()
 def setsettings():
     ########
     ########
     ########
     pass
 
+@cli.command()
 def dish():
     ########
     ########
     ########
     pass
 
+@cli.command()
 def setcron():
-    ########
-    ########
-    ########
-    pass
+    ### if no settings folder then error out
+    #######
+    #######
+    #######
+    #######
+    curr_cron = crontab.CronTab(user=True)
+    if len(list(curr_cron.find_comment("trashgourmandcron"))) > 0:
+        pass # do nothing
+    else:
+        job = curr_cron.new(command="trashgourmand dish", comment="trashgourmandcron")
+        job.hour.on(15)
+        curr_cron.write_to_user(user=True)
 
 if __name__ == "__main__":
     ######
