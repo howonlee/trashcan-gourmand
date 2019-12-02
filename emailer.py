293b2a9ad5ede8eaa08e2f6ba49537a196663a91
howon lee <hlee.howon@gmail.com>
2019-12-01 19:26:26 -0800

    progress

--- emailer.py
+++ emailer.py
@@ -48,11 +48,3 @@ def filter_by_filetype(member: str, filetypes: Union[str, List[str]]):
     else:
         raise Exception("Wrong type on filetype")
 
-#### set settings
-#### get settings
-#### set the cronjob
-
-if __name__ == "__main__":
-    settings = some shit
-    set_curr_img(settings)
-    send_message(datetime.datetime.today(), settings)
---
+++ trash_cli.py
@@ -0,0 +1,15 @@
+
+
+#### set settings
+#### get settings
+#### choose random file and email
+#### set the cronjob
+
+if __name__ == "__main__":
+    ######
+    ######
+    ######
+    ######
+    settings = some shit
+    set_curr_img(settings)
+    send_message(datetime.datetime.today(), settings)
